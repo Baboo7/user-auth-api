ee1996df0c2091e362d6b286a6aeaab496f03bc9
Baboo7 <dbsharp404@gmail.com>
2019-09-03 16:57:05 -0400

    Hash users password

--- src/database/repositories/UserRepository.ts
+++ src/database/repositories/UserRepository.ts
@@ -3,16 +3,17 @@ import { find } from "lodash";
 
 import { IUser } from "../../types";
 
+// Mock table of users
 const USERS: IUser[] = [
   {
     admin: false,
     email: "user@example.com",
-    password: "soleil123"
+    password: "$2b$10$B0VaGQAPeGA/B29sTAg53eDG.eqIHveUthPN6HFFxqiBCwMeSD2vS" // password: soleil123
   },
   {
     admin: true,
     email: "admin@example.com",
-    password: "admin"
+    password: "$2b$10$qcDSijJ3L5PR8c9iu64Bqe9LtF2ryFk0mNXEp3.l0Qzxns2SCke/." // password: admin
   }
 ];
 
